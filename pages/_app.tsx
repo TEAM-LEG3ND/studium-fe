bcc68689a353af4ed144ac8e1b3d74ceee19a318
dkile <dkile532@naver.com>
2023-04-30 11:40:17 +0900

    Apply Noto Sans KR font as default and Source Sans Pro for sub

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/base/globals.sass";
 import type { AppProps } from "next/app";
 
+import { noto_sans_kr, source_sans_pro } from "@/utils/fonts";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${noto_sans_kr.className} ${source_sans_pro.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
