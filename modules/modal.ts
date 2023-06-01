8eb2ef531c9413a98e5227358a9aa86112e1b0a4
라스타_유진 <susanna1221@afreecatv.com>
2023-06-01 11:56:25 +0900

    Modify code style

--- modules/modal.ts
+++ modules/modal.ts
@@ -6,17 +6,14 @@ const initialState: modalState = {
     login: {
         isShow: false,
         prevModal: '',
-        data: {}
     },
     join: {
         isShow: false,
         prevModal: '',
-        data: {}
     },
     joinCompelete: {
         isShow: false,
         prevModal: '',
-        data: {}
     },
 }
 
@@ -44,11 +41,9 @@ export const modalSlice = createSlice({
             const type = action.payload;
             state[type].isShow = false;
         },
-        nextModal(state, action: PayloadAction<{prev: modalType, next: modalType, data: object}>) {
-            const {prev, next, data} = action.payload;
-            state[prev].data = {...state[prev].data, ...data};
-            state[next].prevModal = prev;
-            state[next].data = data;
+        nextModal(state, action: PayloadAction<{current: modalType, next: modalType, data: object}>) {
+            const {current, next, data} = action.payload;
+            state[next].prevModal = current;
         },
 	},
 });
--- types/modal.ts
+++ types/modal.ts
@@ -2,7 +2,6 @@ export type modalType = 'login' | 'join' | 'joinCompelete';
 export type eachModalState = {
     isShow: boolean;
     prevModal: modalType | '';
-    data: object;
 }
 export type modalState = {
     [key in modalType] : eachModalState
