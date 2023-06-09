1dd0e04c5313a26c211a644eebb3cfbcc082793d
dkile <dkile532@naver.com>
2023-06-09 15:54:07 +0900

    Define home api response type

---
+++ types/home.ts
@@ -0,0 +1,24 @@
+// 최초 홈 진입 통 API
+// https://api.server.d0lim.com/studium/v1/home
+
+export type HomeResponse = {
+  polular_recruit_articles: HomeRecruitArticleResponse[];
+} & RecruitArticlesResponse;
+
+// 추가 게시글 요청
+// https://api.server.d0lim.com/studium/v1/home/article?last={oldest_recent_article_id}&size={size}&sort={sortBy}
+export type RecruitArticlesResponse = {
+  recruit_articles: HomeRecruitArticleResponse[];
+  last_recruit_article_id: number;
+};
+
+type HomeRecruitArticleResponse = {
+  id: number;
+  title: string;
+  description: string;
+  tags: string[];
+  created_at: Date;
+  expires_at: Date;
+};
+
+// https://api.server.d0lim.com/studium/v1/recruit/{id}
