24eb351a9583caa394c933e8855b54f9001dc7f7
dkile <dkile532@naver.com>
2023-07-15 13:03:09 +0900

    Move ProfileBtn, ProfileSubmenu to profile dir and extract avatar icon

--- components/profile/ProfileBtn.tsx
+++ components/profile/ProfileBtn.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-
 import styles from "@/styles/components/ProfileBtn.module.sass";
+import Avatar from "../common/icon/Avatar";
 
 type ProfileBtnProps = {
   handleClick: () => void;
@@ -15,13 +14,7 @@ function ProfileBtn({ handleClick }: ProfileBtnProps) {
         aria-label="profile dropdown button"
         className={styles.profileBtn}
       >
-        <Image
-          src="https://studium-fe.s3.ap-northeast-2.amazonaws.com/public/icon/avatar.svg"
-          alt="profile_icon"
-          width={36}
-          height={36}
-          aria-hidden
-        />
+        <Avatar />
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
