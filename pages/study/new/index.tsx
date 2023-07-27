18d9033aab6a3f7b307cf94e0dd8d7889a47c39b
celi1004 <celi1004@cau.ac.kr>
2023-07-27 23:10:54 +0900

    mui 컴포넌트 제거

--- pages/study/new/index.tsx
+++ pages/study/new/index.tsx
@@ -16,7 +16,7 @@ export interface StudyForm {
   studyPlace: "online" | "offline";
   offlinePlace?: string;
   questionnaires: Array<string>;
-}  // TODO: 백엔드랑 상의 후 네이밍, 타입 재설정 필요
+} // TODO: 백엔드랑 상의 후 네이밍, 타입 재설정 필요
 
 const initialStudyForm: StudyForm = {
   title: "",
@@ -31,20 +31,26 @@ const initialStudyForm: StudyForm = {
   member: 0,
   studyPlace: "online",
   questionnaires: [""],
-}
+};
 
 export default function StudyNew() {
   const [studyForm, setStudyForm] = useState<StudyForm>(initialStudyForm);
 
   function handleAddRuleBox() {
     if (studyForm.rules.length < 5) {
-      setStudyForm((studyForm => ({...studyForm, rules: [...studyForm.rules, ""]})))
+      setStudyForm(studyForm => ({
+        ...studyForm,
+        rules: [...studyForm.rules, ""],
+      }));
     }
   }
 
   function handleAddQuestionBox() {
     if (studyForm.questionnaires.length < 5) {
-      setStudyForm((studyForm => ({...studyForm, questionnaires: [...studyForm.questionnaires, ""]})))
+      setStudyForm(studyForm => ({
+        ...studyForm,
+        questionnaires: [...studyForm.questionnaires, ""],
+      }));
     }
   }
 
@@ -66,22 +72,21 @@ export default function StudyNew() {
       <div className={styles.formContainer}>
         <div>
           이름
-          <input type="text" placeholder="스터디의 이름을 적어주세요"/>
+          <input type="text" placeholder="스터디의 이름을 적어주세요" />
         </div>
         <div>
           한 줄 소개
-          <input type="text" placeholder="스터디를 가장 잘 표현할 수 있는 문장을 한 줄로 소개해주세요"/>
+          <input
+            type="text"
+            placeholder="스터디를 가장 잘 표현할 수 있는 문장을 한 줄로 소개해주세요"
+          />
         </div>
         <div>
           <div className={styles.withConstraintForm}>
-            <div>
-              태그
-            </div>
-            <div className={styles.maxCountText}>
-              (최대 5개)
-            </div>
+            <div>태그</div>
+            <div className={styles.maxCountText}>(최대 5개)</div>
           </div>
-          <input type="text" placeholder="autocomplete 컴포넌트로 대체 필요"/>
+          <input type="text" placeholder="autocomplete 컴포넌트로 대체 필요" />
         </div>
         <div>
           스터디 진행 방식
@@ -89,57 +94,84 @@ export default function StudyNew() {
         </div>
         <div>
           <div className={styles.withConstraintForm}>
-            <div>
-              진행 규칙
-            </div>
-            <div className={styles.maxCountText}>
-              (최대 5개)
-            </div>
+            <div>진행 규칙</div>
+            <div className={styles.maxCountText}>(최대 5개)</div>
           </div>
-          { studyForm.rules.map((rule, index) => <input key={index} type="text" placeholder="스터디 진행에 필요한 규칙들을 작성해주세요."/>)}
-          { studyForm.rules.length < 5 && <button className={styles.addInputBox} onClick={handleAddRuleBox}>+</button>}
+          {studyForm.rules.map((rule, index) => (
+            <input
+              key={index}
+              type="text"
+              placeholder="스터디 진행에 필요한 규칙들을 작성해주세요."
+            />
+          ))}
+          {studyForm.rules.length < 5 && (
+            <button className={styles.addInputBox} onClick={handleAddRuleBox}>
+              +
+            </button>
+          )}
         </div>
         <div className={styles.columnFlexForm}>
           <div>
             모집 기간
-            <input type="date"/>
+            <input type="date" />
           </div>
           <div>
             진행 기간
-            <input type="date"/>
+            <input type="date" />
           </div>
         </div>
         <div className={styles.columnFlexForm}>
           <div>
             모집 인원
-            <input type="number" placeholder="최대 정원을 입력해주세요. "/>
+            <input type="number" placeholder="최대 정원을 입력해주세요. " />
           </div>
           <div>
             진행 장소
             <div className={styles.studyPlaceForm}>
               <div className={styles.studyPlaceRadioConainer}>
-                  <label><input type="radio" name="studyPlace" id="online" value='online'/>온라인</label>
-                  <label><input type="radio" name="studyPlace" id="offline" value='offline'/>오프라인</label>
+                <label>
+                  <input
+                    type="radio"
+                    name="studyPlace"
+                    id="online"
+                    value="online"
+                  />
+                  온라인
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name="studyPlace"
+                    id="offline"
+                    value="offline"
+                  />
+                  오프라인
+                </label>
               </div>
-              <input type="text" placeholder="오프라인 장소를 입력해주세요."/>
+              <input type="text" placeholder="오프라인 장소를 입력해주세요." />
             </div>
           </div>
         </div>
         <div>
           <div className={styles.withConstraintForm}>
-            <div>
-              지원자에게 궁금한 점
-            </div>
-            <div className={styles.maxCountText}>
-              (최대 5개)
-            </div>
+            <div>지원자에게 궁금한 점</div>
+            <div className={styles.maxCountText}>(최대 5개)</div>
           </div>
-          { 
-            studyForm.questionnaires.map((questionnaire, index) => 
-              <input key={index} type="text" placeholder="지원 시 지원자가 답변할 질문을 입력해주세요."/>
-            )
-          }
-          { studyForm.questionnaires.length < 5 && <button className={styles.addInputBox} onClick={handleAddQuestionBox}>+</button>}
+          {studyForm.questionnaires.map((questionnaire, index) => (
+            <input
+              key={index}
+              type="text"
+              placeholder="지원 시 지원자가 답변할 질문을 입력해주세요."
+            />
+          ))}
+          {studyForm.questionnaires.length < 5 && (
+            <button
+              className={styles.addInputBox}
+              onClick={handleAddQuestionBox}
+            >
+              +
+            </button>
+          )}
         </div>
       </div>
     </div>
