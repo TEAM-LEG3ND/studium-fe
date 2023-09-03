80a0130af1a7f3f97c5c302eb68908dea864575a
dkile <dkile532@naver.com>
2023-09-04 01:47:58 +0900

    Change leader field in study detail type

--- types/study.ts
+++ types/study.ts
@@ -35,9 +35,8 @@ export type StudyDetail = {
   tags: Tag[];
   leader: {
     id: number;
-    name: string;
+    nickname: string;
     intro: string;
-    thumnail: string;
-    email: string;
+    profileUrl: string;
   };
 };
